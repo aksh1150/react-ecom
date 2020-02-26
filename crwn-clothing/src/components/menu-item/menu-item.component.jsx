e86015fbaa43ab50f6419fba98d839ac1c9d5741
Akshit <akshit@Akshits-iMac.local>
2020-02-25 22:05:25 -0500

    Create new components and background image and pass dynamically classname

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+import "./menu-item.styles.scss";
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    // add inline style as javascript object
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    // pass prop value in className
+    className={`${size} menu-item`}
+  >
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
