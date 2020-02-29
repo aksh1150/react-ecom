e091fc6e7d46cbe281a30c473ba62965fc362e6a
Akshit <akshit@Akshits-iMac.local>
2020-02-29 10:05:41 -0500

    updating our app with a shop page

--- crwn-clothing/src/components/preview-collecton/collection-preview.component.jsx
+++ crwn-clothing/src/components/preview-collecton/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.comoponent";
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -6,9 +7,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, idx) => idx < 10)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
         ))}
     </div>
   </div>
